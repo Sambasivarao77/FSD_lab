3c70e26db34d41cba60b606c903ce451d238691d
Sambasivarao77 <ssrao8ssrao@gmail.com>
2026-07-17 21:46:42 +0530

    Handling Routes

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+//student route
+app.get("/student", (req, res) => {
+    res.send("Student Page");
+});
+
+//teacher route
+app.get("/teacher", (req, res) => {
+    res.send("Teacher Page");
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
